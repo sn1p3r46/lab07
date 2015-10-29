77ecbf9261cac0b57418bad0819d5c136770c822
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-10-29 16:56:14 +0100

    database is fixed (500 error is not shown when server is started again)

--- Examples/src/introsde/rest/ehealth/resources/PersonResource.java
+++ Examples/src/introsde/rest/ehealth/resources/PersonResource.java
@@ -16,95 +16,87 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-
-@Stateless
-@LocalBean
+@Stateless // only used if the the application is deployed in a Java EE container
+@LocalBean // only used if the the application is deployed in a Java EE container
 public class PersonResource {
-	@Context
-	UriInfo uriInfo;
-	@Context
-	Request request;
-	
-	EntityManager entityManager;
-	
-	int id;
+    @Context
+    UriInfo uriInfo;
+    @Context
+    Request request;
+    int id;
 
-	public PersonResource(UriInfo uriInfo, Request request,int id, EntityManager em) {
-		this.uriInfo = uriInfo;
-		this.request = request;
-		this.id = id;
-		this.entityManager = em;
-	}
-	
-	public PersonResource(UriInfo uriInfo, Request request,int id) {
-		this.uriInfo = uriInfo;
-		this.request = request;
-		this.id = id;
-	}
+    EntityManager entityManager; // only used if the application is deployed in a Java EE container
 
-	
-	// Application integration
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Person getPerson() {
-		Person person = this.getPersonById(id);
-		if (person == null)
-			throw new RuntimeException("Get: Person with " + id + " not found");
-		return person;
-	}
+    public PersonResource(UriInfo uriInfo, Request request,int id, EntityManager em) {
+        this.uriInfo = uriInfo;
+        this.request = request;
+        this.id = id;
+        this.entityManager = em;
+    }
 
-	// for the browser
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public Person getPersonHTML() {
-		Person person = this.getPersonById(id);
-		if (person == null)
-			throw new RuntimeException("Get: Person with " + id + " not found");
-		System.out.println("Returning person... " + person.getIdPerson());
-		return person;
-	}
+    public PersonResource(UriInfo uriInfo, Request request,int id) {
+        this.uriInfo = uriInfo;
+        this.request = request;
+        this.id = id;
+    }
 
-	@PUT
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response putPerson(Person person) {
-		System.out.println("--> Updating Person... " +this.id);
-		System.out.println("--> "+person.toString());
-		Person.updatePerson(person);
-		
-		Response res;
-		
-		Person existing = getPersonById(this.id);
-		
-		if (existing == null) {
-			res = Response.noContent().build();
-		} else {
-			res = Response.created(uriInfo.getAbsolutePath()).build();
-			person.setIdPerson(this.id);
-			Person.updatePerson(person);
-		}
+    // Application integration
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Person getPerson() {
+        Person person = this.getPersonById(id);
+        if (person == null)
+            throw new RuntimeException("Get: Person with " + id + " not found");
+        return person;
+    }
 
-		return res;
+    // for the browser
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public Person getPersonHTML() {
+        Person person = this.getPersonById(id);
+        if (person == null)
+            throw new RuntimeException("Get: Person with " + id + " not found");
+        System.out.println("Returning person... " + person.getIdPerson());
+        return person;
+    }
 
-		
-	}
+    @PUT
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response putPerson(Person person) {
+        System.out.println("--> Updating Person... " +this.id);
+        System.out.println("--> "+person.toString());
+        Person.updatePerson(person);
+        Response res;
+        Person existing = getPersonById(this.id);
 
-	@DELETE
-	public void deletePerson() {
-		Person c = getPersonById(id);
-		if (c == null)
-			throw new RuntimeException("Delete: Person with " + id
-					+ " not found");
+        if (existing == null) {
+            res = Response.noContent().build();
+        } else {
+            res = Response.created(uriInfo.getAbsolutePath()).build();
+            person.setIdPerson(this.id);
+            Person.updatePerson(person);
+        }
+        return res;
+    }
 
-		Person.removePerson(c);
-	}
+    @DELETE
+    public void deletePerson() {
+        Person c = getPersonById(id);
+        if (c == null)
+            throw new RuntimeException("Delete: Person with " + id
+                    + " not found");
+        Person.removePerson(c);
+    }
 
-	
-	public Person getPersonById(int personId) {
-		System.out.println("Reading person from DB with id: "+personId);
-		//Person person = entityManager.find(Person.class, personId);
-		
-		Person person = Person.getPersonById(personId);
-		System.out.println("Person: "+person.toString());
-		return person;
-	}
+    public Person getPersonById(int personId) {
+        System.out.println("Reading person from DB with id: "+personId);
+
+        // this will work within a Java EE container, where not DAO will be needed
+        //Person person = entityManager.find(Person.class, personId); 
+
+        Person person = Person.getPersonById(personId);
+        System.out.println("Person: "+person.toString());
+        return person;
+    }
 }
